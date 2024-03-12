50eec6a1f84559bdf1e79d72d57ee3058cc32da4
Zach Silberstein <zach.silberstein@gmail.com>
2024-03-12 15:33:00 -0400

    Add move_robot function
    This functions updates the position of the robot and all legs to the
    correct positions based on the given joint angles.

--- robot_gait_vis/simulate.py
+++ robot_gait_vis/simulate.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+import numpy as np
 
 from robot_gait_vis.robot import Robot
 
@@ -6,14 +7,38 @@ from robot_gait_vis.robot import Robot
 class Simulate():
     """Class to simulate the movement of a Robot."""
 
-    def __init__(self, robot: Robot, dt: float) -> None:
+    def __init__(self,
+                 robot: Robot,
+                 dt: float,
+                 init_thetas: Dict[str, List[float]]) -> None:
         """
         Args:
             robot (Robot): Robot to simulate.
             dt (float): Time between each robot command in seconds.
+            init_thetas (Dict[str, List[float]]): Dictionary of initial joint 
+            angles for each leg on the robot.
+
+        Raises:
+            KeyError: Raises error if not all legs of the robot are given 
+            initial joint angles.
         """
         self.robot = robot
         self.dt = dt
+
+        for leg_name in self.robot.leg_names:
+            if init_thetas.get(leg_name):
+                self._update_leg_pos(leg_name, init_thetas[leg_name])
+            else:
+                raise KeyError(
+                    f'Leg {leg_name} was not given initial joint angles')
+
+        # Ground depth is at lowest leg in Z direction
+        self.ground = np.min([self.robot.legs_pos_global[leg_name][0][-1][2]
+                              for leg_name in self.robot.legs_pos_global])
+        for leg_name in self.robot.leg_names:
+            self._update_leg_down(leg_name)
+        # Increase number of frames
+        self.robot.frames += 1
 
     def move_robot(self, thetas: Dict[str, List[float]]) -> None:
         """Updates the position of the robot given joint angles for the legs.
@@ -22,7 +47,94 @@ class Simulate():
             thetas (Dict[str, List[float]]): Dictionary of joint angles for 
             each leg to move to in radians.
         """
-        return
+        num_legs_stance = 0
+        for leg_name in self.robot.leg_names:
+            if thetas.get(leg_name):
+                self._update_leg_pos(leg_name, thetas[leg_name])
+                self._update_leg_down(leg_name)
+
+            # If no joint angles for leg are provided, leave leg at last position
+            else:
+                self.robot.joint_angles[leg_name].append(
+                    self.robot.joint_angles[leg_name][-1])
+                self.robot.legs_pos_local[leg_name].append(
+                    self.robot.legs_pos_local[leg_name][-1])
+                self.robot.legs_pos_global[leg_name].append(
+                    self.robot.legs_pos_global[leg_name][-1])
+                self.robot.leg_down[leg_name].append(
+                    self.robot.leg_down[leg_name][-1])
+
+            # Stance occurs when leg was on ground and is still on ground
+            if self.robot.leg_down[leg_name][-1] \
+                    and self.robot.leg_down[leg_name][-2]:
+                num_legs_stance += 1
+
+        translate = 0
+        for leg_name in self.robot.leg_names:
+            # If stance occurred
+            if self.robot.leg_down[leg_name][-1] \
+                    and self.robot.leg_down[leg_name][-2]:
+                stance_dist = np.subtract(
+                    self.robot.legs_pos_local[leg_name][-1][-1],
+                    self.robot.legs_pos_local[leg_name][-2][-1])
+                # Stance in opposite direction of motion, total body
+                #   translation is average of stance distance by each leg
+                #   that is in stance
+                translate += -1 * stance_dist / num_legs_stance
+
+        # Update the leg attachment and leg points
+        #   to move with the body
+        self._translate_robot(translate)
+        # Increase number of frames
+        self.robot.frames += 1
+
+    def _update_leg_pos(self, leg_name: str, joint_angles: List[float]):
+        """Updates the history of a leg with the given joint angles.
+
+        Args:
+            leg_name (str): Key for the leg name in the robot.
+            joint_angles (List[float]): Joint angles to move the leg to.
+        """
+        self.robot.joint_angles[leg_name].append(joint_angles)
+        # Local frame is found with forward kinematics
+        local_pos = self.robot.leg_type.forward_kinematics(joint_angles)
+        self.robot.legs_pos_local[leg_name].append(local_pos)
+
+        # Global position is found by moving from
+        #   local frame base to leg attachment point
+        global_pos = [np.add(local,
+                             self.robot.leg_attach[leg_name][-1]).tolist()
+                      for local in local_pos]
+        self.robot.legs_pos_global[leg_name].append(global_pos)
+
+    def _update_leg_down(self, leg_name: str):
+        """Updates the leg_down property.
+
+        Args:
+            leg_name (str): Key for the leg name in the robot.
+        """
+        # Give a buffer to the ground
+        global_pos = self.robot.legs_pos_global[leg_name][-1]
+        down = global_pos[-1][2] - self.ground < 0.001
+        self.robot.leg_down[leg_name].append(down)
+
+    def _translate_robot(self, distance: List[float]):
+        """Translates the robot by the given distance.
+
+        Args:
+            distance (List[float]): Vector to translate the robot as 
+            (X, Y, Z) in meters.
+        """
+        new_pos = np.add(distance, self.robot.body_pos[-1])
+        self.robot.body_pos.append(new_pos.tolist())
+        for leg_name in self.robot.leg_names:
+            self.robot.leg_attach[leg_name].append(
+                np.add(self.robot.leg_attach[leg_name][-1],
+                       distance).tolist())
+            for i in range(self.robot.leg_type.num_joints):
+                global_pos = self.robot.legs_pos_global[leg_name][-1][i]
+                self.robot.legs_pos_global[leg_name][-1][i] = \
+                    np.add(distance, global_pos).tolist()
 
     def animate(self, save_file: str):
         """Animates the history of the robot.
--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,8 +1,11 @@
+from typing import Tuple, List, Dict
 import unittest
+import numpy as np
 
 from robot_gait_vis.robot import Robot
 from robot_gait_vis.planar_leg import PlanarLeg
 from robot_gait_vis.simulate import Simulate
+from robot_gait_vis import gait_gen
 
 
 class TestSimulate(unittest.TestCase):
@@ -12,12 +15,168 @@ class TestSimulate(unittest.TestCase):
         # Make a biped with PlanarLegs for testing
         planar_leg = PlanarLeg([1, 2])
         self.planar_robot = Robot([1, 1, 1], planar_leg, 2)
-        self.sim = Simulate(self.planar_robot, 0.5)
 
+        # Create gait for robot
+        stance = 0.5
+        height = 0.5
+        start_thetas = {}
+        gait_height = self.planar_robot.leg_type.forward_kinematics(
+            np.deg2rad([45, 45]))[-1]
+        for leg_name in self.planar_robot.leg_names:
+            start_thetas[leg_name] = self.planar_robot.leg_type.inverse_kinematics(
+                [stance, 0, gait_height[2]])[0]
+        # Create ellipse trajectory with 2 points and 0.5 duty factor
+        stance_vec = [-stance, 0, 0]
+        height_vec = [stance/2, 0, height]
+        paths = gait_gen.create_ellipse_trajectory(self.planar_robot,
+                                                   start_thetas,
+                                                   stance_vec,
+                                                   height_vec,
+                                                   stance_vec,
+                                                   height_vec,
+                                                   2,
+                                                   0.5)
+        stance_start = {'L0': 0, 'R0': 0.5}
+        self.gait = gait_gen.get_gait(paths, stance_start)
+
+        # Make sim
+        self.sim = Simulate(self.planar_robot, 0.5, self.gait[0])
 
     def test_init(self):
+        # Test that an instance of the Simulate class has been made
+        self.assertIsInstance(self.sim, Simulate)
         self.assertEqual(self.sim.dt, 0.5)
         self.assertIsInstance(self.sim.robot, Robot)
 
+        # Test joint angles
+        joints = {'L0': [self.gait[0]['L0']], 'R0': [self.gait[0]['R0']]}
+        self.compare_dict_list_list(self.sim.robot.joint_angles, joints)
+
+        # Test local positions
+        local = {'L0':
+                 [self.planar_robot.leg_type.forward_kinematics(
+                     self.gait[0]['L0'])],
+                 'R0':
+                 [self.planar_robot.leg_type.forward_kinematics(
+                     self.gait[0]['R0'])]}
+        self.compare_dict_list_list_tuple(self.sim.robot.legs_pos_local,
+                                          local)
+
+        # Test global positions
+        global_leg = {'L0': [], 'R0': []}
+        for leg, pos in global_leg.items():
+            if leg[0] == 'R':
+                y = -0.5
+            else:
+                y = 0.5
+            for point in local[leg]:
+                temp = list(point)
+                for i, t in enumerate(temp):
+                    inner = list(t)
+                    inner[1] = y
+                    temp[i] = inner
+                pos.append(temp)
+        self.compare_dict_list_list_tuple(
+            self.sim.robot.legs_pos_global, global_leg)
+
+        # Test leg attachment positions
+        leg_attach = {'L0': [[0, 0.5, 0]],
+                      'R0': [[0, -0.5, 0]]}
+        self.assertEqual(self.sim.robot.leg_attach, leg_attach)
+
+        # Test the body position
+        body = [[0, 0, 0]]
+        self.assertAlmostEqual(self.sim.robot.body_pos, body)
+
+        # Test ground depth
+        depth = self.planar_robot.leg_type.forward_kinematics(
+            np.deg2rad([45, 45]))[-1][2]
+        self.assertEqual(self.sim.ground, depth)
+
+        # Test leg down
+        down = {'L0': [True], 'R0': [True]}
+        self.assertEqual(self.sim.robot.leg_down, down)
+
+        self.assertEqual(self.sim.robot.frames, 1)
+
     def test_move_robot(self):
-        pass
+        # Move the robot
+        for thetas in self.gait[1:]:
+            self.sim.move_robot(thetas)
+
+        # Test joint angles
+        joints = {'L0': [self.gait[i]['L0'] for i in range(len(self.gait))],
+                  'R0': [self.gait[i]['R0'] for i in range(len(self.gait))]}
+        self.compare_dict_list_list(self.sim.robot.joint_angles, joints)
+
+        # Test local positions
+        local = {'L0':
+                 [self.planar_robot.leg_type.forward_kinematics(self.gait[i]['L0'])
+                  for i in range(len(self.gait))],
+                 'R0':
+                 [self.planar_robot.leg_type.forward_kinematics(self.gait[i]['R0'])
+                  for i in range(len(self.gait))]}
+        self.compare_dict_list_list_tuple(self.sim.robot.legs_pos_local, local)
+
+        # Test global positions
+        global_leg = {'L0': [], 'R0': []}
+        for leg, pos in global_leg.items():
+            if leg[0] == 'R':
+                y = -0.5
+            else:
+                y = 0.5
+            for point in local[leg]:
+                temp = list(point)
+                for i, t in enumerate(temp):
+                    inner = list(t)
+                    inner[1] = y
+                    temp[i] = inner
+                pos.append(temp)
+        self.compare_dict_list_list_tuple(
+            self.sim.robot.legs_pos_global, global_leg)
+
+        # Test leg attachment positions
+        leg_attach = {'L0': [[0, 0.5, 0], [0, 0.5, 0]],
+                      'R0': [[0, -0.5, 0], [0, -0.5, 0]]}
+        self.assertEqual(self.sim.robot.leg_attach, leg_attach)
+
+        # Test the body position
+        body = [[0, 0, 0], [0, 0, 0]]
+        self.assertAlmostEqual(self.sim.robot.body_pos, body)
+
+        # Test leg down
+        down = {'L0': [True, True], 'R0': [True, True]}
+        self.assertEqual(self.sim.robot.leg_down, down)
+
+        self.assertEqual(self.sim.robot.frames, 2)
+
+    def compare_dict_list_list(self,
+                               d1: Dict[str, List[List[float]]],
+                               d2: Dict[str, List[List[float]]],
+                               decimal=10) -> None:
+        self.assertEqual(len(d1), len(d2),
+                         f'List 1 has {len(d1)} keys but List 2 has {len(d2)} keys')
+        for key in d1:
+            self.assertTrue(key in d2, f'Key {key} is not in both dicts')
+            self.assertEqual(len(d1[key]), len(d2[key]))
+            for i in range(len(d1[key])):
+                self.assertEqual(len(d1[key][i]), len(d2[key][i]))
+                np.testing.assert_almost_equal(d1[key][i], d2[key][i], decimal,
+                                               f'Lists with key {key}, {i} are not almost equal')
+
+    def compare_dict_list_list_tuple(self,
+                                     d1: Dict[str, List[List[Tuple[float, ...]]]],
+                                     d2: Dict[str, List[List[Tuple[float, ...]]]],
+                                     decimal=10) -> None:
+        self.assertEqual(len(d1), len(d2),
+                         f'List 1 has {len(d1)} keys but List 2 has {len(d2)} keys')
+        for key in d1:
+            self.assertTrue(key in d2, f'Key {key} is not in both dicts')
+            self.assertEqual(len(d1[key]), len(d2[key]))
+            for i in range(len(d1[key])):
+                self.assertEqual(len(d1[key][i]), len(d2[key][i]))
+                for j in range(len(d1[key][i])):
+                    self.assertEqual(len(d1[key][i][j]), len(d2[key][i][j]))
+                    np.testing.assert_almost_equal(d1[key][i][j], d2[key][i][j],
+                            decimal,
+                            f'Tuples at index {key}, {i}, {j} are not almost equal')
