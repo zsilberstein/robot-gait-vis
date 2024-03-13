9047641ef28f50b6f2542da8eb88f2644d7434cc
Zach Silberstein <zach.silberstein@gmail.com>
2024-03-13 16:15:39 -0400

    Add animate function
    This function animates the history of the robot and saves the animation
    as a .gif or .mp4 file.

--- robot_gait_vis/simulate.py
+++ robot_gait_vis/simulate.py
@@ -1,6 +1,7 @@
 from typing import List, Dict
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 
 from robot_gait_vis.robot import Robot
 
@@ -54,7 +55,8 @@ class Simulate():
                 self._update_leg_pos(leg_name, thetas[leg_name])
                 self._update_leg_down(leg_name)
 
-            # If no joint angles for leg are provided, leave leg at last position
+            # If no joint angles for leg are provided,
+            #   leave leg at last position
             else:
                 self.robot.joint_angles[leg_name].append(
                     self.robot.joint_angles[leg_name][-1])
@@ -89,7 +91,9 @@ class Simulate():
         # Increase number of frames
         self.robot.frames += 1
 
-    def _update_leg_pos(self, leg_name: str, joint_angles: List[float]):
+    def _update_leg_pos(self,
+                        leg_name: str,
+                        joint_angles: List[float]) -> None:
         """Updates the history of a leg with the given joint angles.
 
         Args:
@@ -108,7 +112,7 @@ class Simulate():
                       for local in local_pos]
         self.robot.legs_pos_global[leg_name].append(global_pos)
 
-    def _update_leg_down(self, leg_name: str):
+    def _update_leg_down(self, leg_name: str) -> None:
         """Updates the leg_down property.
 
         Args:
@@ -119,7 +123,7 @@ class Simulate():
         down = global_pos[-1][2] - self.ground < 0.001
         self.robot.leg_down[leg_name].append(down)
 
-    def _translate_robot(self, distance: List[float]):
+    def _translate_robot(self, distance: List[float]) -> None:
         """Translates the robot by the given distance.
 
         Args:
@@ -137,20 +141,189 @@ class Simulate():
                 self.robot.legs_pos_global[leg_name][-1][i] = \
                     np.add(distance, global_pos).tolist()
 
-    def animate(self, save_file: str):
+    def animate(self, save_file: str) -> None:
         """Animates the history of the robot.
 
         Args:
             save_file (str): A .gif or a .mp4 file to save animation to.
+
+        Raises:
+            ValueError: Raises error if file type is not .gif or .mp4.
         """
-        return
+        if save_file[-3:] == 'mp4':
+            writer = animation.FFMpegWriter(fps=1/self.dt)
+        elif save_file[-3:] == 'gif':
+            writer = animation.PillowWriter(fps=1/self.dt)
+        else:
+            raise ValueError('File must be either .gif or .mp4')
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection="3d", computed_zorder=False)
+
+        # Find plot limits
+        lims = []
+        for i in range(3):
+            joints = [self.robot.legs_pos_global[leg_name][frame][joint][i]
+                      for joint in range(self.robot.leg_type.num_joints)
+                      for frame in range(self.robot.frames)
+                      for leg_name in self.robot.leg_names]
+            leg_attach = [self.robot.leg_attach[leg_name][frame][i]
+                          for frame in range(self.robot.frames)
+                          for leg_name in self.robot.leg_names]
+            low = np.min(joints+leg_attach)
+            # Don't lower ground
+            if i < 2:
+                low -= self.robot.body_dims[i]
+            high = np.max(joints+leg_attach) + self.robot.body_dims[i]
+            lims.append([low, high])
+        ax.xaxis.set_major_locator(plt.MaxNLocator(nbins=4))
+        ax.yaxis.set_major_locator(plt.MaxNLocator(nbins=4))
+        ax.zaxis.set_major_locator(plt.MaxNLocator(nbins=4))
+        ax.set(xlim=lims[0],
+               ylim=lims[1],
+               zlim=lims[2],
+
+               xlabel='X [m]',
+               ylabel='Y [m]',
+               zlabel='Z [m]',
+               title=f'Time = {0}s',
+               aspect='equal')
+
+        # Plot ground
+        x_grid_ground, y_grid_ground = np.meshgrid(lims[0], lims[1])
+        z_grid_ground = np.array([self.ground]*4).reshape(2, 2)
+        ax.plot_surface(x_grid_ground,
+                        y_grid_ground,
+                        z_grid_ground,
+                        alpha=1,
+                        color='C0')
+
+        # Fill in leg coordinates and plot
+        legs = []
+        for leg_name in self.robot.leg_names:
+            leg_pos = self._get_leg_positions(leg_name, 0)
+            # Plot right legs further out, hide left legs behind body
+            if leg_name[0] == 'R':
+                legs.append(ax.plot(leg_pos[0],
+                                    leg_pos[1],
+                                    leg_pos[2],
+                                    color='C3',
+                                    zorder=3,
+                                    linewidth=2)[0])
+            else:
+                legs.append(ax.plot(leg_pos[0],
+                                    leg_pos[1],
+                                    leg_pos[2],
+                                    color='C3',
+                                    zorder=1,
+                                    linewidth=2)[0])
+        # Plot body
+        box_surfaces = []
+        surfs = self._get_box_surfaces(0)
+        for surf in surfs:
+            x_grid, y_grid, z_grid = surf
+            box_surfaces.append(ax.plot_surface(x_grid,
+                                                y_grid,
+                                                z_grid,
+                                                color='k',
+                                                alpha=0.75,
+                                                zorder=2))
+
+        def update(frame):
+            # Update title
+            ax.set(title=f'Time = {self.dt*frame: .3f}s')
+            # Update legs
+            for i, leg_name in enumerate(self.robot.leg_names):
+                leg_pos = self._get_leg_positions(leg_name, frame)
+                legs[i].set_xdata(leg_pos[0])
+                legs[i].set_ydata(leg_pos[1])
+                legs[i].set_3d_properties(leg_pos[2])
+            # Update body
+            surfs = self._get_box_surfaces(frame)
+            for i, surf in enumerate(surfs):
+                box_surfaces[i].remove()
+                x_grid, y_grid, z_grid = surf
+                box_surfaces[i] = ax.plot_surface(x_grid,
+                                                  y_grid,
+                                                  z_grid,
+                                                  color='k',
+                                                  alpha=0.75,
+                                                  zorder=2)
+
+            return (legs, box_surfaces)
+
+        # Animate and save
+        ani = animation.FuncAnimation(fig=fig,
+                                      func=update,
+                                      frames=self.robot.frames,
+                                      interval=self.dt*1000)
+        ani.save(save_file, writer=writer)
+        plt.close()
+
+    def _get_leg_positions(self,
+                           leg_name: str,
+                           frame: int) -> List[List[float]]:
+        """Transforms positions of the joints of a leg for plotting.
+
+        Args:
+            leg_name (str): Name of leg to compute leg positions for.
+            frame (int): Frame to compute leg positions for.
+
+        Returns:
+            List[List[float]]: List of length 3 specifying the x, y, z 
+            positions of the leg attachment point and each joint on leg. 
+        """
+        leg_pos = []
+        # For x, y, z
+        for i in range(3):
+            legs = [self.robot.leg_attach[leg_name][frame][i]] \
+                + [self.robot.legs_pos_global[leg_name][frame][j][i]
+                   for j in range(self.robot.leg_type.num_joints)]
+            leg_pos.append(legs)
+        return leg_pos
+
+    def _get_box_surfaces(self, frame: int) -> List[List[List[float]]]:
+        """Generates six surfaces to be plotted that represent the body of 
+        the robot.
+
+        Args:
+            frame (int): Frame to plot the body.
+
+        Returns:
+            List[List[List[float]]]: List of surfaces to be plotted.
+        """
+        body_pos = self.robot.body_pos[frame]
+        body_dims = self.robot.body_dims
+        # Determine the max and min of the body in x, y, z
+        ranges = []
+        for i in range(3):
+            range_i = [body_pos[i]-body_dims[i]/2, body_pos[i]+body_dims[i]/2]
+            ranges.append(range_i)
+
+        surfaces = []
+        x_order = [0, 0, 1]
+        y_order = [1, 2, 2]
+        z_order = [2, 1, 0]
+
+        for x, y, z in zip(x_order, y_order, z_order):
+            a_grid, b_grid = np.meshgrid(ranges[x], ranges[y])
+            # Get the two parallel surfaces (top and bottom / left and right)
+            for i in range(2):
+                c_grid = np.array([ranges[z][i]]*4).reshape(2, 2)
+                temp = [0, 0, 0]
+                temp[x] = a_grid
+                temp[y] = b_grid
+                temp[z] = c_grid
+                surfaces.append(temp)
+
+        return surfaces
 
     def plot_xyz_history(self) -> None:
         """Plots the position of the robot center of 
             body and the end-effector of each leg over time.
         """
         # Left to right is X, Y, Z vs time
-        _, ax = plt.subplots(ncols=3, nrows=1)
+        fig, ax = plt.subplots(ncols=3, nrows=1)
         plt.subplots_adjust(left=0.05,
                             bottom=0.05,
                             right=0.95,
@@ -182,13 +355,14 @@ class Simulate():
                       ylabel=f'{chr(ord("X")+i)} position (m)')
         # Plot legend on X plot
         ax[0].legend(loc="upper left")
+        fig.suptitle('Position vs Time')
         plt.show()
 
     def plot_joint_history(self) -> None:
         """Plots the joint angles of each leg over time."""
         # Left to right joint1, joint2, ... , jointN
         num_joints = self.robot.leg_type.num_joints
-        _, ax = plt.subplots(ncols=num_joints, nrows=1)
+        fig, ax = plt.subplots(ncols=num_joints, nrows=1)
         plt.subplots_adjust(left=0.05,
                             bottom=0.05,
                             right=0.95,
@@ -200,15 +374,16 @@ class Simulate():
         # For each joint
         for i in range(num_joints):
             for leg_name in self.robot.leg_names:
-                p, = ax[i].plot(time,
-                                [self.robot.joint_angles[leg_name][j][i]
-                                 for j in range(self.robot.frames)],
-                                label=leg_name,
-                                linestyle='--',
-                                marker='o')
+                ax[i].plot(time,
+                           [self.robot.joint_angles[leg_name][j][i]
+                            for j in range(self.robot.frames)],
+                           label=leg_name,
+                           linestyle='--',
+                           marker='o')
 
             ax[i].set(xlabel='Time (s)',
                       ylabel=f'Joint {i+1} (radians)')
         # Plot legend on X plot
         ax[0].legend(loc="upper left")
+        fig.suptitle('Joint Angles vs Time')
         plt.show()
